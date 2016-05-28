8d9df474554a2c66c9cdab89eaab154d7dec258d
DiverFictionB <xboxsecret4@gmail.com>
2016-05-28 18:31:03 +0300

    First Commit

---
+++ GitHubExperimentAli/GitHubExperimentAli.cpp
@@ -0,0 +1,13 @@
+// GitHubExperimentAli.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	cout << "hello gitHub"<<endl;
+	system("pause");
+    return 0;
+}
+
